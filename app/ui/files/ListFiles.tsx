5ec29ca3419436338e2fe512d7a9f0cb850d9ff4
mardinw <mardinw@outlook.com>
2023-12-12 10:15:12 +0700

    initial commit

--- app/ui/files/ListFiles.tsx
+++ app/ui/files/ListFiles.tsx
@@ -1,6 +1,7 @@
 import { appInfo } from "@/app/config/appInfo"
 import { useEffect, useState } from "react";
 import { FileList } from "@/app/lib/dpt/definitions";
+import PopUpTablePreview from "../popup/PopUpTablePreview";
 
 export default function ListFiles() {
   const apiUrl = appInfo.apiDomain
